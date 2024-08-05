04f24b167921c0f4d16935b2b8ef9f7f6198e31b
PedrOliveiraM <pedropucmont@gmail.com>
2024-08-05 18:53:31 -0300

    finish list-thoughts

--- src/app/components/thought/list-thoughts/list-thoughts.component.ts
+++ src/app/components/thought/list-thoughts/list-thoughts.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ThoughtCardComponent } from '../thought-card/thought-card.component';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-list-thoughts',
   standalone: true,
-  imports: [RouterLink, ThoughtCardComponent, NgFor],
+  imports: [RouterLink, ThoughtCardComponent, NgFor, NgIf],
   templateUrl: './list-thoughts.component.html',
   styleUrl: './list-thoughts.component.css',
 })
 export class ListThoughtsComponent {
-  listThought = [];
+  listThought = [
+    {
+      id: '1',
+      conteudo: 'excel avançado',
+      autoria: 'hastag treinamentos',
+      modelo: 'modelo2',
+    },
+    {
+      id: '2',
+      conteudo: 'Power BI',
+      autoria: 'hastag treinamentos',
+      modelo: 'modelo3',
+    },
+  ];
 }
